34ea81ab73fea1d8ad1c0dcef1b82ac5ea712d90
Mark Garcia <mymggithub@users.noreply.github.com>
2022-10-06 18:17:29 -0500

    cors

--- backend/models/api/controllers/TwitterController.php
+++ backend/models/api/controllers/TwitterController.php
@@ -8,6 +8,24 @@ use \backend\models\api\Api;
 
 class TwitterController extends \yii\web\Controller
 {
+    public function behaviors()
+    {
+        return array_merge(parent::behaviors(), [
+
+            // For cross-domain AJAX request
+            'corsFilter'  => [
+                'class' => \yii\filters\Cors::className(),
+                'cors'  => [
+                    // restrict access to domains:
+                    'Origin'                           => static::allowedDomains(),
+                    'Access-Control-Request-Method'    => ['POST'],
+                    'Access-Control-Allow-Credentials' => true,
+                    'Access-Control-Max-Age'           => 3600,                 // Cache (seconds)
+                ],
+            ],
+
+        ]);
+    }
     public function actionIndex()
     {
         echo "Welcome to the index";
